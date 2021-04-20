5bd6607465b9b7d94c250207a1f674b591ee89b0
juma-moringa <allan.juma@student.moringaschool.com>
2021-04-20 15:17:19 +0300

    changes on the html

--- JS/scripts.js
+++ JS/scripts.js
@@ -21,15 +21,13 @@ $(document).ready(function () {
         var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
         $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
-        $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
-        $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+        $(".contact").last().click(function() {
+          $("#show-contact").show();
+          $("#show-contact h2").text(newContact.firstName);
+          $(".first-name").text(newContact.firstName);
+          $(".last-name").text(newContact.lastName);
+        });  
         $("input#new-first-name").val("");
         $("input#new-last-name").val("");
     });
 });
-$(".contact").last().click(function() {
-    $("#show-contact").show();
-    $("#show-contact h2").text(newContact.firstName);
-    $(".first-name").text(newContact.firstName);
-    $(".last-name").text(newContact.lastName);
-  });  
